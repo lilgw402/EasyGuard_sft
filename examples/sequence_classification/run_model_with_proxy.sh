d73c1cea0de6071fe8f5beaa020c8f7481924bfe
xiaochen.qiu <xiaochen.qiu@bytedance.com>
2022-10-14 17:36:05 +0800

    update proxy

--- examples/sequence_classification/run_model_with_proxy.sh
+++ examples/sequence_classification/run_model_with_proxy.sh
@@ -3,6 +3,7 @@
 # export no_proxy=code.byted.org
 
 export http_proxy=http://10.20.47.147:3128 https_proxy=https://10.20.47.147:3128 export no_proxy=code.byted.org
+# export http_proxy=http://sys-proxy-rd-relay.byted.org:8118 https_proxy=http://sys-proxy-rd-relay.byted.org:8118 export no_proxy=byted.org
 
 python3 run_model.py --config config_roberta.yaml
 
