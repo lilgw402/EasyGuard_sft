43fbed5bb05fdc6d910a941adadee54c9945096c
姜军军 <jiangjunjun.happy@bytedance.com>
2023-01-12 19:10:17 +0800

    add fashionxlm

--- easyguard/modelzoo/models/mdeberta_v2/__init__.py
+++ easyguard/modelzoo/models/mdeberta_v2/__init__.py
@@ -1,2 +1,7 @@
-from .modeling_deberta_v2 import DebertaV2ForMaskedLM, DebertaV2Model, \
-    DebertaV2ForSequenceClassification, DebertaV2ForQuestionAnswering, DebertaV2ForTokenClassification
+from .modeling_deberta_v2 import (
+    DebertaV2ForMaskedLM,
+    DebertaV2Model,
+    DebertaV2ForSequenceClassification,
+    DebertaV2ForQuestionAnswering,
+    DebertaV2ForTokenClassification
+)
