7c69b7a13858f91783988d13170cd4681e6d7461
dongjunwei <dongjunwei@bytedance.com>
2023-01-31 20:54:10 +0800

    test

--- easyguard/__init__.py
+++ easyguard/__init__.py
@@ -4,13 +4,13 @@ import os
 
 __version__ = "0.0.2"
 
+from .core import AutoImageProcessor, AutoModel, AutoProcessor, AutoTokenizer
+from .modelzoo.config import MODELZOO_CONFIG
 from .utils import EASYGUARD_CACHE
 
 # set the easyguard cache directory
-os.environ["EASYGUARD_CACHE"] = EASYGUARD_CACHE
+# os.environ["EASYGUARD_CACHE"] = EASYGUARD_CACHE
 
-from .core import AutoImageProcessor, AutoModel, AutoProcessor, AutoTokenizer
-from .modelzoo.config import MODELZOO_CONFIG
 
 # This line will be programatically read/write by setup.py.
 # Leave them at the bottom of this file and don't touch them.
