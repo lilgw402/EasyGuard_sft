7315a6c4fd282e326750a2884968860346d5a923
wanli.0815 <wanli.0815@bytedance.com>
2023-02-23 15:35:03 +0800

    update run pretrain code

--- examples/fashion_deberta/run_pretrain.sh
+++ examples/fashion_deberta/run_pretrain.sh
@@ -1,1 +1,2 @@
-python3 run_pretrain.py --config config/pretrain_default_config.yaml
+# python3 run_pretrain.py --config config/pretrain_default_config.yaml
+python3 run_pretrain.py --config config/pretrain_mlc_config.yaml
