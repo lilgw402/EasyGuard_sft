176fad7058a9e8e2293bf0888c9964cc11665ec8
王贤 <wangxian.137@bytedance.com>
2023-03-05 23:50:03 +0800

    timeout 1

--- easyguard/appzoo/framealbert_classification/data.py
+++ easyguard/appzoo/framealbert_classification/data.py
@@ -189,7 +189,7 @@ class TorchvisionLabelDataset(DistLineReadingDataset):
                     try:
                         for url in data_item['images']:
                             # image_str = download_url_with_exception(get_original_url(url), timeout=3)
-                            image_str = download_image_to_base64(get_original_url(url), timeout=3)
+                            image_str = download_image_to_base64(get_original_url(url), timeout=1)
                             # image_str = download_image_to_base64(get_real_url(url), timeout=2)
                             if image_str != b'' and image_str != '':
                                 try:
