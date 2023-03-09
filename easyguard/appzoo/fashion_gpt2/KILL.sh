ae17489cee7f78e16314487d30ec31ef5ba94e88
doushihan <doushihan@bytedance.com>
2023-03-09 15:55:23 +0800

    delete files in appzoo

--- easyguard/appzoo/fashion_gpt2/KILL.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Trying to kill processes matching keyword $1"
-ps -ef | grep "$1" | grep -v grep | awk '{print $2}' | xargs kill -9
--- easyguard/appzoo/fashion_gpt2/mariana/README.md
+++
@@ -1,3 +0,0 @@
-# Mariana package
-
-This folder is for common shared utils that usually don't change across experiments, such as data and evaluators.
