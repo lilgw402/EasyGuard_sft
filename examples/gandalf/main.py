21a1f1332345f50a29175555e40080bd104cfa3f
jiangxubin <jiangxubin@bytedance.com>
2023-03-12 17:01:01 +0800

    [Update]Beta version of gandalf with sciprt

--- examples/gandalf/main.py
+++ examples/gandalf/main.py
@@ -15,7 +15,6 @@ from utils.util import load_conf, load_from_yaml,load_from_tcc,load_from_bbc,che
 from utils.file_util import hmkdir, check_hdfs_exist
 
 def prepare_folder(config):
-    # print(config.trainer)
     os.makedirs(config.trainer.default_root_dir, exist_ok=True)
     if not check_hdfs_exist(config.trainer.default_hdfs_dir):
             hmkdir(config.trainer.default_hdfs_dir)
@@ -23,19 +22,27 @@ def prepare_folder(config):
 def prepare_gandalf_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", type=str, help="local yaml conf")
-    # Extra comand line option
     parser.add_argument("--fit", action="store_true")
     parser.add_argument("--val", action="store_true")
     parser.add_argument("--trace", action="store_true")
     args, config_override = parser.parse_known_args()
-    cruise_args = [arg for arg in sys.argv[1:] if len(re.findall('fit|val|trace',arg))==0]
-    sys.argv = [sys.argv[0]]+cruise_args
+    sys.argv  = [arg for arg in sys.argv if len(re.findall('--fit|--val|--trace',arg))==0]
     if args.config:
         config = Dict(load_from_yaml(args.config))
     else:
         raise FileNotFoundError('config file option must be specified')
     config.update({'fit':args.fit,'val':args.val, 'trace':args.trace})
     return Dict(config)
+
+def prepare_common_trainer_defaults():
+    trainer_defaults = {
+                        'seed':42,
+                        'precision':16,
+                        'summarize_model_depth':3,
+                        'enable_checkpoint': True,
+                        'checkpoint_monitor': 'loss',
+                        'checkpoint_mode': 'min'}
+    return trainer_defaults
 
 def prepare_trainer_components(config):
     model_module = get_model_module(config['model']['type'])
@@ -45,7 +52,8 @@ def prepare_trainer_components(config):
 def main():
     config = prepare_gandalf_args()
     model_module, data_module = prepare_trainer_components(config)
-    cli = CruiseCLI(model_module,data_module,trainer_class=CruiseTrainer)
+    trainer_defaults = prepare_common_trainer_defaults()
+    cli = CruiseCLI(model_module,data_module,trainer_class=CruiseTrainer,trainer_defaults=trainer_defaults)
     cfg, trainer, model, datamodule = cli.parse_args()
     print_cfg(cfg)
     if config['fit']:
--- examples/gandalf/run_on_merlin.sh
+++ examples/gandalf/run_on_merlin.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-basedir=`pwd -P`
-echo 'Curdir: '$basedir
-# curfile=$0
-# echo 'CurFile: '$curfile
-grandparentdir="$(cd ../../; pwd)"
-echo 'grandparentdir: '$grandparentdir
+ROOT_HOME=`pwd -P`
+echo 'Curdir: '$ROOT_HOME
+project_dir="$(cd examples/gandalf/; pwd)"
+echo 'ProjectDir: '$project_dir
+cd $project_dir
+cur_dir=`pwd -P`
+echo 'Curdir: '$cur_dir
 echo 'GPU NUM: '$ARNOLD_WORKER_GPU
 
-export PYTHONPATH=$PYTHONPATH:$grandparent_dir
 export PYTHONPATH=$PYTHONPATH:/opt/tiger
 export PYTHONPATH=$PYTHONPATH:/opt/tiger/anyon2
 export PYTHONPATH=$PYTHONPATH:/opt/tiger/titan
 export PYTHONPATH=$PYTHONPATH:/opt/tiger/cruise
 export PYTHONPATH=$PYTHONPATH:/opt/tiger/easyguard
+
+
 
 # # pretrain weights
 echo 'pulling down pretrained weights...'
@@ -20,12 +22,16 @@ if [ ! -d "models/weights" ]; then mkdir models/weights; fi
 if [ ! -d "models/weights/fashion_deberta_asr" ]; then hadoop fs -get  hdfs:///user/jiangxubin/models/pretrain/fashion_deberta_asr ./models/weights; fi
 
 # # trigger train proc
+export ROOT_HOME=${ROOT_HOME:=$ROOT_HOME}
+export PROJECT_HOME=${PROJECT_HOME:=$project_dir}
 export CRUISE_HOME=${CRUISE_HOME:=/opt/tiger/cruise}
-export GANDALF_HOME=${GANDALF_HOME:=$basedir}
 
+echo 'ROOT_HOME: '$ROOT_HOME
+echo 'PROJECT_HOME: '$PROJECT_HOME
 echo 'CRUISE_HOME: '$CRUISE_HOME
+
 if [ -f "$CRUISE_HOME/cruise/tools/TORCHRUN" ]; then
-  $CRUISE_HOME/cruise/tools/TORCHRUN  $GANDALF_HOME/main.py $@
+  $CRUISE_HOME/cruise/tools/TORCHRUN  $PROJECT_HOME/main.py $@
 else
   echo 'CRUISE_HOME not exist'
 fi
