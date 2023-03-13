bb5b541cf436b6ef77992f0457fbf11020716af5
wanli.0815 <wanli.0815@bytedance.com>
2023-03-13 15:31:52 +0800

    change code

--- examples/fashion_gpt2/run_instruct_inference_interactive.sh
+++ examples/fashion_gpt2/run_instruct_inference_interactive.sh
@@ -3,5 +3,6 @@ python3 tasks/gpt2/unsup/model.py \
       --generate-temp=0.7 \
       --model=tasks/gpt2/unsup/1b3_v1.yaml \
       --generate-steps=1 \
+      --generate-trial-num=1 \
       --model.partial_pretrain=/mnt/bn/wanli/experiments/instruct_tunning/outputs/20230311-1353/checkpoints/global_step_57800/mp_rank_00_model_states.pt \
       --data.tokenizer=hdfs://haruna/home/byte_ecom_govern/user/doushihan/tokenizer/tokenizer/zh_0620_newcut_caster_145665_lowercase
