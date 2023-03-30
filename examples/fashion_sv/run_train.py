ef2a981c844102ff587b766c460a233f4dfcdcb2
王贤 <wangxian.137@bytedance.com>
2023-03-31 00:34:20 +0800

    init

---
+++ examples/fashion_sv/run_train.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+# sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", ".."))
+try:
+    import easyguard
+except ImportError:
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", ".."))
+
+import random
+
+import numpy as np
+import torch
+from cruise import CruiseCLI, CruiseTrainer
+from examples.fashion_sv.data import SVDataModule
+from examples.fashion_sv.model import FashionSV
+
+rand_seed = 42
+
+
+def setup_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    try:
+        torch.backends.cudnn.deterministic = True
+    except:
+        ...
+
+
+# random seed
+setup_seed(rand_seed)
+cli = CruiseCLI(
+    SVDataModule,
+    trainer_class=CruiseTrainer,
+    datamodule_class=FashionSV,
+    trainer_defaults={
+        "summarize_model_depth": 2,
+    },
+)
+cfg, trainer, model, datamodule = cli.parse_args()
+
+trainer.fit(model, datamodule)
