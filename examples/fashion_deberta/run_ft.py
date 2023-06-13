5837a74dfab013f546c788b9ec62a1aedeab19b3
yangzheming <yangzheming@bytedance.com>
2023-06-13 21:15:19 +0800

    fashion_deberta: add __main__ and ch default params

--- examples/fashion_deberta/run_ft.py
+++ examples/fashion_deberta/run_ft.py
@@ -13,20 +13,21 @@ from easyguard.appzoo.fashion_deberta.data_finetune import FashionDataFtModule
 from easyguard.utils.arguments import print_cfg
 
 
-cli = CruiseCLI(FashionDebertaFtModel,
-                trainer_class=CruiseTrainer,
-                datamodule_class=FashionDataFtModule,
-                trainer_defaults={
-                    'max_epochs': 2,
-                    'val_check_interval': [3000, 1.0],
-                    'summarize_model_depth': 2,
-                    'checkpoint_monitor': 'total_auc_score',
-                    'checkpoint_mode': 'max',
-                    'precision': 'fp16',
-                    'enable_versions': True,
-                    'default_root_dir': '/mnt/bd/yangzheming/cruise/cruise_logs',
-                    'default_hdfs_dir': 'hdfs://haruna/home/byte_ecom_govern/user/yangzheming/chinese/common_model/trails/common_ft/asr_example/model_outputs'
-                    })
-cfg, trainer, model, datamodule = cli.parse_args()
-print_cfg(cfg)
-trainer.fit(model, datamodule)
+if __name__ == '__main__':
+    cli = CruiseCLI(FashionDebertaFtModel,
+                    trainer_class=CruiseTrainer,
+                    datamodule_class=FashionDataFtModule,
+                    trainer_defaults={
+                        'max_epochs': 2,
+                        'val_check_interval': [3000, 1.0],
+                        'summarize_model_depth': 2,
+                        'checkpoint_monitor': 'total_auc_score',
+                        'checkpoint_mode': 'max',
+                        'precision': 'fp16',
+                        'enable_versions': True,
+                        'default_root_dir': '/mnt/bd/yangzheming/cruise/cruise_logs',
+                        'default_hdfs_dir': 'hdfs://haruna/home/byte_ecom_govern/user/yangzheming/chinese/common_model/trails/common_ft/asr_example/model_outputs'
+                        })
+    cfg, trainer, model, datamodule = cli.parse_args()
+    print_cfg(cfg)
+    trainer.fit(model, datamodule)
--- examples/fashion_deberta/run_pretrain.py
+++ examples/fashion_deberta/run_pretrain.py
@@ -19,21 +19,23 @@ warning: 如果计算auc score，记得验证集里需要包含所有label，比
 
 """
 
-cli = CruiseCLI(FashionDebertaModel,
-                trainer_class=CruiseTrainer,
-                datamodule_class=FashionDataModule,
-                trainer_defaults={
-                    'max_epochs': 6,
-                    'val_check_interval': [10000, 1.0],
-                    'summarize_model_depth': 2,
-                    'checkpoint_monitor': 'val_loss',
-                    'checkpoint_mode': 'min',
-                    'precision': 'fp16',
-                    'enable_versions': True,
-                    'resume_ckpt_path': 'auto',
-                    'default_root_dir': '/mnt/bd/yangzheming/cruise/cruise_logs',
-                    'default_hdfs_dir': 'hdfs://haruna/home/byte_ecom_govern/user/yangzheming/chinese/common_model/trails/ccr_v3_1_live_0.3b_mlm_cl/model_outputs'
-                    })
-cfg, trainer, model, datamodule = cli.parse_args()
-print_cfg(cfg)
-trainer.fit(model, datamodule)
+
+if __name__ == '__main__':
+    cli = CruiseCLI(FashionDebertaModel,
+                    trainer_class=CruiseTrainer,
+                    datamodule_class=FashionDataModule,
+                    trainer_defaults={
+                        'max_epochs': 6,
+                        'val_check_interval': [10000, 1.0],
+                        'summarize_model_depth': 2,
+                        'checkpoint_monitor': 'val_loss',
+                        'checkpoint_mode': 'min',
+                        'precision': 'fp16',
+                        'enable_versions': True,
+                        'resume_ckpt_path': 'auto',
+                        'default_root_dir': '/mnt/bd/yangzheming/cruise/cruise_logs',
+                        'default_hdfs_dir': 'hdfs://haruna/home/byte_ecom_govern/user/yangzheming/chinese/common_model/trails/ccr_v3_1_live_0.3b_mlm_cl/model_outputs'
+                        })
+    cfg, trainer, model, datamodule = cli.parse_args()
+    print_cfg(cfg)
+    trainer.fit(model, datamodule)
