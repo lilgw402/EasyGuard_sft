1c8376dc94651f059e38296e25d79114bd0cff95
王贤 <wangxian.137@bytedance.com>
2023-08-30 23:33:41 +0800

    quick_demo

--- examples/eva_vit_giant/quick_demo.py
+++ examples/eva_vit_giant/quick_demo.py
@@ -1,11 +1,10 @@
+import io
+
 import torch
+from PIL import Image
 from torchvision import transforms
 
 from easyguard import AutoModel
-
-
-def _convert_to_rgb(image):
-    return image.convert("RGB")
 
 
 def img_processer():
@@ -16,7 +15,6 @@ def img_processer():
         [
             transforms.Resize(224, interpolation=transforms.InterpolationMode.BICUBIC),
             transforms.CenterCrop(224),
-            _convert_to_rgb,
             transforms.ToTensor(),
             normalize,
         ]
@@ -27,8 +25,16 @@ def img_processer():
 vit_giant = AutoModel.from_pretrained("eva_vit_giant")
 
 procosser = img_processer()
-rand_input = torch.randn(1, 3, 224, 224)
-p_image = procosser(rand_input)
+black_image = (
+    b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x03\x00\x00\x00\x03\x08\x02\x00\x00\x00"
+    b'\xd9J"\xe8\x00\x00\x00\x12IDAT\x08\x1dcd\x80\x01F\x06\x18`d\x80\x01\x00\x00Z\x00'
+    b"\x04we\x03N\x00\x00\x00\x00IEND\xaeB`\x82"
+)
+img = Image.open(io.BytesIO(black_image)).convert("RGB")
+black_img = procosser(img)
+black_input = black_img.reshape(1, 3, 224, 224)
 
-out = vit_giant(p_image)
+rand_input = torch.randn(1, 3, 224, 224)
+
+out = vit_giant(black_input)
 print(out)
