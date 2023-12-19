320e916cac623c9f5ebe01aabd21e1e4772cc1d5
wuji.0306 <wuji.0306@bytedance.com>
2023-12-19 11:58:55 +0800

    infer---

--- examples/valley/valley/inference/multi_infer.sh
+++ examples/valley/valley/inference/multi_infer.sh
@@ -2,7 +2,7 @@ for num in {45000..60000..5000}
 do  
     torchrun --nproc_per_node $ARNOLD_WORKER_GPU --nnodes $ARNOLD_WORKER_NUM --node_rank=$ARNOLD_ID --master_addr $ARNOLD_WORKER_0_HOST \
     --master_port 12701 valley/inference/inference_valley_jinshou.py --model-class valley-product \
-    --model-name /mnt/bn/yangmin-priv-fashionmm/Data/wuji/data_process/new_process/product_checkpoints/data-wj-$1-valley-product-7b-jinshou-class-lora-multi-class/checkpoint-$num \
+    --model-name /mnt/bn/yangmin-priv-fashionmm/Data/wuji/data_process/new_process/product_checkpoints/data-mutli-$1-valley-7b-jinshou-class-lora-multi-class
     --data_path /mnt/bn/yangmin-priv-fashionmm/Data/wuji/wupian_process/new_wupian/wupian_test_data_4w_ocr512_n_multi_ans_small_model_valley_product.json \
     --image_folder /mnt/bn/yangmin-priv-fashionmm/Data/yangshuang/jinshou_benchmark_image_data \
     --out_path /mnt/bn/yangmin-priv-fashionmm/Data/wuji/wupian_process/output/data-wj-$1-valley-product-7b-jinshou-class-lora-multi-class-test-ocr512-$num.txt \
