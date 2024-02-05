23e613ec5f31c4433b01816b08fccfbb607cf379
wuji.0306 <wuji.0306@bytedance.com>
2024-02-05 20:21:12 +0800

    infer

--- examples/valley/valley/inference/multi_infer_2.sh
+++ examples/valley/valley/inference/multi_infer_2.sh
@@ -1,15 +1,23 @@
 
 
-for num in {30000..30000..5000}
-do  
-    torchrun --nproc_per_node $ARNOLD_WORKER_GPU --nnodes $ARNOLD_WORKER_NUM --node_rank=$ARNOLD_ID --master_addr $ARNOLD_WORKER_0_HOST \
+torchrun --nproc_per_node $ARNOLD_WORKER_GPU --nnodes $ARNOLD_WORKER_NUM --node_rank=$ARNOLD_ID --master_addr $ARNOLD_WORKER_0_HOST \
     --master_port 12701 valley/inference/inference_valley_jinshou.py --model-class valley-product \
-    --model-name /mnt/bn/yangmin-priv-fashionmm/Data/wuji/data_process/new_process/product_checkpoints/data-mutli-$1-valley-7b-jinshou-class-lora-multi-class/checkpoint-$num \
-    --data_path /mnt/bn/yangmin-priv-fashionmm/Data/wuji/wupian_process/new_wupian/wupian_test_data_4w_new_v18_valley_product.json \
+    --model-name /mnt/bn/yangmin-priv-fashionmm/Data/wuji/data_process/new_process/product_checkpoints/data-mutli-v21-valley-7b-jinshou-class-lora-multi-class/checkpoint-10000 \
+    --data_path/mnt/bn/yangmin-priv-fashionmm/Data/wuji/wupian_process/new_wupian/test_0201_add_feature_valley_product.json \
     --image_folder /mnt/bn/yangmin-priv-fashionmm/Data/yangshuang/jinshou_benchmark_image_data \
-    --out_path /mnt/bn/yangmin-priv-fashionmm/Data/wuji/wupian_process/output/data-wj-$1-valley-product-7b-jinshou-class-lora-multi-class-test-0109-$num.txt \
-    --DDP --prompt_version jinshou_cot
-done
+    --out_path /mnt/bn/yangmin-priv-fashionmm/Data/wuji/wupian_process/output/data-wj-v21-valley-product-7b-jinshou-class-lora-multi-class-10000.txt \
+    --DDP --prompt_version total
+
+# for num in {30000..30000..5000}
+# do  
+#     torchrun --nproc_per_node $ARNOLD_WORKER_GPU --nnodes $ARNOLD_WORKER_NUM --node_rank=$ARNOLD_ID --master_addr $ARNOLD_WORKER_0_HOST \
+#     --master_port 12701 valley/inference/inference_valley_jinshou.py --model-class valley-product \
+#     --model-name /mnt/bn/yangmin-priv-fashionmm/Data/wuji/data_process/new_process/product_checkpoints/data-mutli-$1-valley-7b-jinshou-class-lora-multi-class/checkpoint-$num \
+#     --data_path /mnt/bn/yangmin-priv-fashionmm/Data/wuji/wupian_process/new_wupian/wupian_test_data_4w_new_v18_valley_product.json \
+#     --image_folder /mnt/bn/yangmin-priv-fashionmm/Data/yangshuang/jinshou_benchmark_image_data \
+#     --out_path /mnt/bn/yangmin-priv-fashionmm/Data/wuji/wupian_process/output/data-wj-$1-valley-product-7b-jinshou-class-lora-multi-class-test-0109-$num.txt \
+#     --DDP --prompt_version jinshou_cot
+# done
 
 # for num in {15000..20000..5000}
 # do  
