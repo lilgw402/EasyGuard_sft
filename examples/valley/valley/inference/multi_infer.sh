788191db87be61f2a2da6ee035b26fb2eaf2228e
wuji.0306 <wuji.0306@bytedance.com>
2024-02-20 22:55:59 +0800

    infer_0220

--- examples/valley/valley/inference/multi_infer.sh
+++ examples/valley/valley/inference/multi_infer.sh
@@ -2,14 +2,14 @@
 
 
 
-for num in {40000..75000..5000}
+for num in {50000..75000..5000}
 do  
     torchrun --nproc_per_node $ARNOLD_WORKER_GPU --nnodes $ARNOLD_WORKER_NUM --node_rank=$ARNOLD_ID --master_addr $ARNOLD_WORKER_0_HOST \
     --master_port 12701 valley/inference/inference_valley_jinshou.py --model-class valley-product \
     --model-name /mnt/bn/yangmin-priv-fashionmm/Data/wuji/data_process/new_process/product_checkpoints/data-mutli-$1-valley-7b-jinshou-class-lora-multi-class/checkpoint-$num \
-    --data_path /mnt/bn/yangmin-priv-fashionmm/Data/wuji/wupian_process/new_wupian/test_0201_valley_product.json \
+    --data_path /mnt/bn/yangmin-priv-fashionmm/Data/wuji/wupian_process/new_wupian/test_0220_valley_product.json \
     --image_folder /mnt/bn/yangmin-priv-fashionmm/Data/yangshuang/jinshou_benchmark_image_data \
-    --out_path /mnt/bn/yangmin-priv-fashionmm/Data/wuji/wupian_process/output/data-wj-$1-valley-product-7b-jinshou-class-lora-multi-class-$num.txt \
+    --out_path /mnt/bn/yangmin-priv-fashionmm/Data/wuji/wupian_process/output/data-wj-$1-0220-valley-product-7b-jinshou-class-lora-multi-class-$num.txt \
     --DDP --prompt_version total
 done
 
